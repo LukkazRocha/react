e965774e5eedaf27965a0c9df9331e1f984b1b1a
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-10-16 20:16:27 -0300

    exercicio01

---
+++ exercicio01/my-app/src/components/Lorem-Ipsum.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LoremIpsum() {
+    return <p>Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss deixa as pessoas mais interessantis.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.Mé faiz elementum girarzis, nisi eros vermeio.Atirei o pau no gatis, per gatis num morreus.</p>
+}
+
+export default LoremIpsum;
