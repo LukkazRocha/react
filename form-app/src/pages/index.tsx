6402915734ce7e056108df69b38d3cf24870b813
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-10-19 17:23:44 -0300

    montando um form

---
+++ form-app/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import { Form } from '../components/Form';
+
+const Home: NextPage = () => {
+  return (
+    <section className="container">
+      
+      <Form />
+    </section>
+  )
+}
+
+export default Home
