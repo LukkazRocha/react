6a87705180f20e9111c9b54c863872742aa2f3cb
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-10-27 12:01:46 -0300

    começando a estilização do formulário

--- formularios/my-form/components/Demo/Demo.tsx
+++ formularios/my-form/components/Demo/Demo.tsx
@@ -1,6 +1,6 @@
 import { InputFloatLabel } from '../InputFloatLabel/index';
 import React, { useState } from "react";
-import { Button, Checkbox, Col, Divider, Form, Input, Row, Select} from 'antd';
+import { Button, Checkbox, Col, Form, Input, Row, Select} from 'antd';
 
 import 'antd/dist/antd.css';
 import styles from './Demo.module.css';
@@ -18,16 +18,20 @@ export const Demo = () => {
   };
   const [firstName, setFirstName] = useState<string>();
   const [lastName, setLastName] = useState<string>();
-  const [organization, setOrganization] = useState<string>();
   const [email, setEmail] = useState<string>();
+  const [phone, setPhone] = useState<string>();
+  const [organization, setOrganization] = useState<string>();
   const [jobTitle, setJobTitle] = useState<string>();
 
-  const { Option } = Select;
+  const { Option } = Select; 
 
-  return (
+  return (          
     <section className={styles.container}>
-      <Form name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>       
-
+      <header className={styles.header}>
+        <h1>Solicite uma demonstração</h1>
+        <h3>Nós te responderemos em até 12h!</h3>
+      </header>
+      <Form name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
           <Row gutter={24}>
             <Col xs={24} sm={12} md={6}>
               <Form.Item name={'firstName'} rules={[{ required: true }]}>
@@ -64,11 +68,16 @@ export const Demo = () => {
           <Col xs={24} sm={12} md={6}>
             <Form.Item>
               <InputFloatLabel placeholder='+55'></InputFloatLabel>
-            </Form.Item>          
+            </Form.Item>
           </Col>
           <Col xs={24} sm={12} md={6}>
-            <Form.Item>
-              <InputFloatLabel placeholder='Phone'></InputFloatLabel>
+            <Form.Item name={'phone'} rules={[{ required: true }]}>
+              <InputFloatLabel 
+                placeholder='Telefone'
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                >
+              </InputFloatLabel>
             </Form.Item>
           </Col>
           <Col xs={24} sm={24} md={12}>
@@ -88,28 +97,59 @@ export const Demo = () => {
               <InputFloatLabel
                 placeholder='Cargo'
                 value={jobTitle}
-                onChange={(e) => setJobTitle(e.target.value)}            
-              >            
+                onChange={(e) => setJobTitle(e.target.value)}
+              >
               </InputFloatLabel>
             </Form.Item>
           </Col>
           <Col xs={24} sm={24} md={12}>
             <Form.Item name={'team'}>
               <Select
+                className={styles.select}
                 placeholder="De qual time você faz parte?"
               >
                 <Option value="businessIntelligence">Inteligência Empresarial</Option>
-                <Option value="dataScience">Ciência de Dados</Option>            
-                <Option value="engineering">Engenheiro</Option>            
-                <Option value="gis">SIG</Option>            
-                <Option value="it">ISTO</Option>            
-                <Option value="other">Outro</Option>            
+                <Option value="dataScience">Ciência de Dados</Option>
+                <Option value="engineering">Engenheiro</Option>
+                <Option value="gis">SIG</Option>
+                <Option value="it">ISTO</Option>
+                <Option value="other">Outro</Option>
               </Select>
           </Form.Item>
           </Col>
         </Row>
-
-      </Form> 
+        <Row>
+          <Col span={24}>
+            <Form.Item name={'description'}>
+              <Input.TextArea className={styles.description}                
+                placeholder='Conte-nos suas ideias'
+              />
+            </Form.Item>
+          </Col>  
+        </Row> 
+        <Row justify="space-between">
+          <Col>
+            <Form.Item
+              name="agreement"
+              valuePropName="checked"                 
+            >
+              <Checkbox>
+                Além da demonstração, desejo receber conteúdos da Datlo
+              </Checkbox>
+            </Form.Item>
+          </Col>
+          <Col>
+            <Form.Item /* wrapperCol={{ offset: 8 }} */>
+              <Button 
+                size='large'
+                type="primary" 
+                htmlType="submit">
+                Solicitar Demo
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>         
+      </Form>      
     </section>    
   )
 }
