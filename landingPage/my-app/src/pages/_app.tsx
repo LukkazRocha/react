192e731b9cef8cd855702456663da247cb70a4e2
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-11-07 13:47:03 -0300

    updated

--- landingPage/my-app/src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app'
-
-import '../styles/globals.css'
-import '../components/InputFloatLabel.css'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
