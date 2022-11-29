19287e614c4c27740700370e3850713a8a1de0cb
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-11-29 17:39:01 -0300

    iniciando estilização do projeto ignews (ignite-2022)

--- rocketseat/ignite-2021/ignews/src/pages/_app.tsx
+++ rocketseat/ignite-2021/ignews/src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Header } from "../components/Header";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import "../styles/global.scss";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp;
